8930c840d012caf5ed04e042df099bdf87208e00
warleon <51892390+warleon@users.noreply.github.com>
2022-06-01 18:29:07 -0500

    add tests

--- src/main.py
+++ src/main.py
@@ -11,25 +11,3 @@ def isOldEnough(birthdate,dateTest):
 		raise Exception("DateException")
 	else:
 		return (diff > 18)
-
-def main():
-	try:
-		print(isOldEnough("2000/01/01","2003/01/01"))#False
-	except Exception as err:#
-		print(err)#
-	try:#
-		print(isOldEnough("2000/01/01","2020/01/01"))#True	
-	except Exception as err:#
-		print(err)#
-	try:#
-		print(isOldEnough("2000/01/01","1940/01/01"))#DateException
-	except Exception as err:#
-		print(err)#
-	try:#
-		print(isOldEnough("2000-01-01","2000-01-01"))#DateFormatException
-	except Exception as err:
-		print(err)
-		
-
-if __name__ == '__main__':
-	main()
--- src/test_main.py
+++ src/test_main.py
@@ -3,8 +3,9 @@ import unittest
 
 class TestIsOldEnough(unittest.TestCase):
 	def test_isOldEnough(self):
-		actual = isOldEnough("2000/01/01","2020/01/01")
-		self.assertEqual(actual, True)
-
-if __name__ == '__main__':
-	unittest.main()
+		self.assertEqual(isOldEnough("2000/01/01","2020/01/01"), True)
+		self.assertEqual(isOldEnough("2000/01/01","2003/01/01"), False)
+		with self.assertRaises(Exception):
+			isOldEnough("2000/01/01","1940/01/01")#DateException
+		with self.assertRaises(Exception):
+			isOldEnough("2000-01-01","2000-01-01")#DateFormatException
