252b2d1ecb57856ffb6515b76c668a194392e1ff
warleon <51892390+warleon@users.noreply.github.com>
2022-05-31 21:30:04 -0500

    funcion

--- src/main.py
+++ src/main.py
@@ -1,5 +1,36 @@
-def main():
+from datetime import datetime
+
+class DateException(Exception):
 	pass
+
+
+def isOldEnough(birthdate,dateTest):
+	d1 = datetime.strptime(birthdate, "%Y/%m/%d")
+	d2 = datetime.strptime(dateTest, "%Y/%m/%d")
+	diff= d2.year - d1.year
+	if(diff<0):
+		raise DateException
+	else:
+		return (diff > 18)
+
+def main():
+	try:
+		print(isOldEnough("2000/01/01","2003/01/01"))#False
+	except Exception as err:#
+		print(err)#
+	try:#
+		print(isOldEnough("2000/01/01","2020/01/01"))#True	
+	except Exception as err:#
+		print(err)#
+	try:#
+		print(isOldEnough("2000/01/01","1940/01/01"))#raise
+	except Exception as err:#
+		print(err)#
+	try:#
+		print(isOldEnough("2000-01-01","2000-01-01"))#raise
+	except Exception as err:
+		print(err)
+		
 
 if __name__ == '__main__':
 	main()
